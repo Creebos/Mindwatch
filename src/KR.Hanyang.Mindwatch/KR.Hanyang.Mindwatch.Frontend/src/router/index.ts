749e28a0a55e1983b02fdc5c3b260e9cd05173ae
LeandroPS7 <leandromiguelsilvestre@gmail.com>
2024-12-03 23:44:53 +0900

    freude

--- src/KR.Hanyang.Mindwatch/KR.Hanyang.Mindwatch.Frontend/src/router/index.ts
+++ src/KR.Hanyang.Mindwatch/KR.Hanyang.Mindwatch.Frontend/src/router/index.ts
@@ -1,14 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
-import EmployeeDetail from "../pages/EmployeeDetail.vue";
-import EmployeeHome from "../pages/EmployeeHome.vue";
+import HomeView from "../views/HomeView.vue";
+import EmployeeView from "../views/EmployeeView.vue";
+import HRManagerView from "../views/HRManagerView.vue";
+import ManagerView from "../views/ManagerView.vue";
+import SurveyForm from "../views/SurveyForm.vue"; // Import the SurveyForm view
 
 const routes = [
-  { path: "/home", name: "Employee Home", component: EmployeeHome },
-  { path: "/employees", name: "Employee Overview", component: EmployeeDetail },
+  { path: "/", name: "Home", component: HomeView },
+  { path: "/employee", name: "Employee", component: EmployeeView },
+  { path: "/hr-manager", name: "HRManager", component: HRManagerView },
+  { path: "/manager", name: "Manager", component: ManagerView },
+  { path: "/surveyform", name: "SurveyForm", component: SurveyForm }, // Add the SurveyForm route
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
