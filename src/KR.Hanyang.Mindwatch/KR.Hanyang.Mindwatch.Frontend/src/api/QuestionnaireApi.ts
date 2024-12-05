66276a6c2a74d257adc8816326fdbee8b8a22bdb
meyeryve <meyeryve@students.zhaw.ch>
2024-12-05 11:45:26 +0900

    fix questionnaire api

--- src/KR.Hanyang.Mindwatch/KR.Hanyang.Mindwatch.Frontend/src/api/QuestionnaireApi.ts
+++ src/KR.Hanyang.Mindwatch/KR.Hanyang.Mindwatch.Frontend/src/api/QuestionnaireApi.ts
@@ -6,37 +6,37 @@ import apiClient from "./http";
 
 export const QuestionnaireAPI = {
   async getAllQuestionnaires(): Promise<Questionnaire[]> {
-    const response = await apiClient.get("/api/questionnaires");
+    const response = await apiClient.get("/questionnaires");
     return response.data.$values as Questionnaire[];
   },
 
   async getQuestionnaireById(id: number): Promise<Questionnaire> {
-    const response = await apiClient.get(`/api/questionnaires/${id}`);
+    const response = await apiClient.get(`/questionnaires/${id}`);
     return response.data as Questionnaire;
   },
 
   async createQuestionnaire(questionnaire: Questionnaire): Promise<Questionnaire> {
-    const response = await apiClient.post("/api/questionnaires", questionnaire);
+    const response = await apiClient.post("/questionnaires", questionnaire);
     return response.data as Questionnaire;
   },
 
   async getQuestionnaireRunById(id: number): Promise<QuestionnaireRun> {
-    const response = await apiClient.get(`/api/questionnaires/questionnaire-runs/${id}`);
+    const response = await apiClient.get(`/questionnaires/questionnaire-runs/${id}`);
     return response.data as QuestionnaireRun;
   },
 
   async createQuestionnaireRun(run: QuestionnaireRun): Promise<QuestionnaireRun> {
-    const response = await apiClient.post("/api/questionnaires/questionnaire-runs", run);
+    const response = await apiClient.post("/questionnaires/questionnaire-runs", run);
     return response.data as QuestionnaireRun;
   },
 
   async submitAnswer(answer: Answer): Promise<Answer> {
-    const response = await apiClient.post("/api/questionnaires/answers", answer);
+    const response = await apiClient.post("/questionnaires/answers", answer);
     return response.data as Answer;
   },
 
   async addQuestion(question: Question): Promise<Question> {
-    const response = await apiClient.post("/api/questionnaires/questions", question);
+    const response = await apiClient.post("/questionnaires/questions", question);
     return response.data as Question;
   },
 };
